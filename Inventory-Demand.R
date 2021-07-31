5e02516448a914ec27961b4222a1617d9812dcb6
Igor Guerato Garbo <igorgueratogarbo@pop-os.localdomain>
2021-07-31 20:03:16 -0300

    evolution in data exploration

--- Inventory-Demand.R
+++ Inventory-Demand.R
@@ -1,7 +1,12 @@
 setwd("~/Documents/Stock-Demand-Prediction")
+
 library(tidyverse)
 library(data.table)
+library(lattice)
+library(corrplot)
+
 df_comp <- fread("train.csv")
+
 head(df_comp)
 
 factors <- c("Agencia_ID", "Canal_ID", "Ruta_SAK",
@@ -23,3 +28,22 @@ df_sample%>%
   ggplot(aes(x = Semana, y = Demanda_Semanal))+
   geom_line()+
   ggtitle("Mudanca de Necessidade de Estoque")
+
+
+#histogramas para cada uma das variaveis inteiras
+
+numeric <- c("Semana","Venta_uni_hoy","Venta_hoy",
+                  "Dev_uni_proxima","Dev_proxima","Demanda_uni_equil")
+
+factors <- c("Agencia_ID", "Canal_ID", "Ruta_SAK",
+             "Producto_ID")
+
+lapply(factors, function(x){
+ggplot(df_sample, aes_string(y="Demanda_uni_equil", x = x))+
+  geom_boxplot()+ ylim(c(0,1750))
+})
+
+corrplot(cor(df_sample[,..numeric]), method = "ellipse")
+
+
+
